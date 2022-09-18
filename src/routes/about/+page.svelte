ee824d2b88519c19e08e9b619573ab56aca0daea
Rijal Hanif <rhizeft@gmail.com>
2022-09-18 21:38:49 +0700

    second commit

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<!-- dulunya ini namanya index.svelte -->
+
+<h1>This is about section</h1>
+<p>so the +page.svelte is added in maybe every section?<br>
+    it's actually quite confusing
+</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
 import adapter from '@sveltejs/adapter-auto';
+import { append } from 'svelte/internal';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
+	
+		// target: '#svelte' -> harusnya ada ini utk nembak src/app.html id svelte
 	}
 };
 
